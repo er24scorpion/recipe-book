f78f97932901aee0f2451ea8691dd67e68f323ec
Angelo <angelos@hoteliga.com>
2021-02-03 14:17:42 +0200

    Updating

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -20,7 +20,7 @@ export class AuthComponent implements OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
-  @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
+  @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
 
   private closeSub: Subscription;
 
