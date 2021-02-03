ceb6c71ab558a06255d399dd9b5327ad2f4b0e18
Angelo <angelos@hoteliga.com>
2021-02-03 13:30:36 +0200

    Formatting

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,18 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import {
   HttpInterceptor,
   HttpRequest,
   HttpHandler,
-  HttpParams
-} from '@angular/common/http';
-import { take, exhaustMap, map } from 'rxjs/operators';
-import { Store } from '@ngrx/store';
-import * as fromApp from '../store/app.reducer';
-import { AuthService } from './auth.service';
+  HttpParams,
+} from "@angular/common/http";
+import { take, exhaustMap, map } from "rxjs/operators";
+import { Store } from "@ngrx/store";
+import * as fromApp from "../store/app.reducer";
+import { AuthService } from "./auth.service";
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
-  constructor(private authService: AuthService, private store: Store<fromApp.AppState>) {}
+  constructor(
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     // return this.authService.user.pipe(
@@ -27,20 +30,20 @@ export class AuthInterceptorService implements HttpInterceptor {
     //     return next.handle(modifiedReq);
     //   })
     // );
-    return this.store.select('auth').pipe(
-        take(1),
-        map(authState => {
-          return authState.user;
-        }),
-        exhaustMap(user => {
-          if (!user) {
-            return next.handle(req);
-          }
-          const modifiedReq = req.clone({
-            params: new HttpParams().set('auth', user.token)
-          });
-          return next.handle(modifiedReq);
-        })
-      );
+    return this.store.select("auth").pipe(
+      take(1),
+      map((authState) => {
+        return authState.user;
+      }),
+      exhaustMap((user) => {
+        if (!user) {
+          return next.handle(req);
+        }
+        const modifiedReq = req.clone({
+          params: new HttpParams().set("auth", user.token),
+        });
+        return next.handle(modifiedReq);
+      })
+    );
   }
 }
