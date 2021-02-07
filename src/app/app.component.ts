79f0334db0d490b274e533758554d82e18ae6084
Angelo <angelos@hoteliga.com>
2021-02-07 19:37:17 +0200

    AngularUniversal, nestjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Store } from "@ngrx/store";
-import { Component, OnInit } from "@angular/core";
+import { Component, Inject, OnInit, PLATFORM_ID } from "@angular/core";
 import { LoggingService } from "./logging.service";
 import * as fromApp from "./store/app.reducer";
 import * as AuthActions from "./auth/store/auth.actions";
+import { isPlatformBrowser } from "@angular/common";
 
 @Component({
   selector: "app-root",
@@ -12,12 +13,15 @@ import * as AuthActions from "./auth/store/auth.actions";
 export class AppComponent implements OnInit {
   constructor(
     private store: Store<fromApp.AppState>,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    @Inject(PLATFORM_ID) private platformId // to check where the app runs (server?)
   ) {}
 
   ngOnInit() {
     // this.authService.autoLogin();
-    this.store.dispatch(AuthActions.autoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(AuthActions.autoLogin());
+    }
     this.loggingService.printLog("Hello from AppComponent ngOnInit");
   }
 }
